0db40fd1bb6665f2e43621db3352335a9acccd00
Andrew Canaday <andrew.canaday@nytimes.com>
2018-05-21 03:19:00 -0400

    Updating main travis build script

--- travis/script.sh
+++ travis/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-GDM_LDFLAGSFLAGS=(
-    "-s -w"
-    "-X main.lbl=$TRAVIS_BRANCH"
-    "-X main.build=$TRAVIS_BUILD_NUMBER"
-    "-X main.rev=$TRAVIS_COMMIT"
-    )
+GDM_LDFLAGS=""
+GDM_LDFLAGS+="-s -w "
+GDM_LDFLAGS+="-X main.lbl=$TRAVIS_BRANCH "
+GDM_LD_FLAGS+="-X main.build=$TRAVIS_BUILD_NUMBER "
+GDM_LD_FLAGS+=" -X main.rev=$TRAVIS_COMMIT"
+
 go vet
 go test github.com/nytimes/drone-gdm/plugin
-go build -ldflags "${GDM_LDFLAGSFLAGS[@]}" -a -tags netgo
+go build -ldflags "${GDM_LDFLAGS}" -a -tags netgo
 
 # EOF
