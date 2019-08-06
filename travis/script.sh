940e158c445a636041eef765a6d9fbcabfbbff7e
andrew-canaday <andrew.canaday@nytimes.com>
2019-08-06 00:13:17 -0400

    Fix typo (no impact on behavior) in travis build script

--- travis/script.sh
+++ travis/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 GDM_LDFLAGS=""
-GDM_LDFLAGS+="-s -w "
-GDM_LDFLAGS+="-X main.lbl=$TRAVIS_BRANCH "
-GDM_LD_FLAGS+="-X main.build=$TRAVIS_BUILD_NUMBER "
-GDM_LD_FLAGS+=" -X main.rev=$TRAVIS_COMMIT"
+GDM_LDFLAGS+=" -s -w"
+GDM_LDFLAGS+=" -X main.lbl=${TRAVIS_BRANCH}"
+GDM_LDFLAGS+=" -X main.build=${TRAVIS_BUILD_NUMBER}"
+GDM_LDFLAGS+=" -X main.rev=${TRAVIS_COMMIT}"
 
 go vet
 go test ./...
